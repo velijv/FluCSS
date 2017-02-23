a671c249276819409e6bf87a28a54c7b88d5a265
Veli-Johan Veromann <velij.v@gmail.com>
2017-02-23 16:13:03 +0200

    Converted to jQuery plugin, minified, added demo html
    + converted script to jQuery plugin, call it from $("html").flucss();
    + added "flucss.min.js" - minified
    + added "index.html" for demo usage

--- flucss.js
+++ flucss.js
@@ -4,7 +4,14 @@
    http://veli.ee/flucss
    ========================================================================== */
 
-$(function(){
+
+   (function ( $ ) {
+   	$.fn.flucss = function( options ) {
+
+        // default options
+        var options = $.extend({
+        	black: false
+        }, options );
 
 // check localstorage support, get state from localstorage to avoid flickering when navigating or reloading
 if (typeof(Storage) !== "undefined") {
@@ -13,14 +20,10 @@ if (typeof(Storage) !== "undefined") {
 		$("html").addClass(storeddaytimename);
 	}
 } else { }
-
-
 // get users location
-function getLocation(){
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(showPosition);
 	} else { } // no geolocation support
-}
 
 // the main function to get active (closest) sun state
 // you MUST INCLUDE https://github.com/mourner/suncalc for calculating the sun states
@@ -47,14 +50,56 @@ function showPosition(position) {
 			}
 		}
 	}
-
 	// adds the appropriate class to root HTML element
 	// add the class ".fullblack" to your "HTML" element, if you want night to be completely black
-	$("html").addClass(daytimename); 
-
 }
 
-// run the function on page load
-getLocation();
+this.each(function() {
+	var element = $( this );
+	element.addClass(localStorage.getItem("daytimename")); 
+	if(options.black){
+		element.addClass("fullblack"); 
+	}
+});
 
-})
+return this;
+
+
+};
+}( jQuery ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
